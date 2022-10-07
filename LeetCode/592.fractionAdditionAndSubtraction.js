392e59351aa5d313fa73b825be62f51980c2d70a
caionikolas <Caio.kanu@hotmail.com>
2022-10-07 02:39:15 -0300

    mmc

--- LeetCode/592.fractionAdditionAndSubtraction.js
+++ LeetCode/592.fractionAdditionAndSubtraction.js
@@ -1,6 +1,27 @@
 var fractionAddition = function(expression) {
   let letras = expression.split('')
   console.log(letras)
+
+  let numeros = []
+  let numerador = []
+  let denominador = []
+
+  for (let i = 0; i < expression.length; i++) {
+    if (isNaN(expression[i]) == false){
+      numeros.push(expression[i])
+    }
+  }
+
+
+  var mmc = 1
+
+  console.log(mmc)
+
+  function e_multiplo(v1, v2){
+    return v1 % v2 === 0
+  }
+
+  console.log(numeros)
 };
 
-console.log(fractionAddition("-1/2+1/2"))
+console.log(fractionAddition("1/3-1/2"))
