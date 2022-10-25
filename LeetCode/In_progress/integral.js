3134992492fe1ef9afabee3772a1c28682967fb2
Caio Nikolas <103952344+caionikolas@users.noreply.github.com>
2022-10-25 17:31:39 -0300

    Add files via upload

---
+++ LeetCode/In_progress/integral.js
@@ -0,0 +1,37 @@
+/*
+var romanToInt = function(s) {
+    let romano = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+    console.log(romano.I)
+    let sum = 0
+    for (let i = 0; i < s.length; i++) {
+        for (let j = 0; j < Object.keys(romano).length; j++) {
+            if (s[i] == romano[j]){
+                sum += romano.
+            }
+        }
+    }
+};
+
+console.log(romanToInt(III))
+
+let romano = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+}
+
+console.log(romano)
+*/
