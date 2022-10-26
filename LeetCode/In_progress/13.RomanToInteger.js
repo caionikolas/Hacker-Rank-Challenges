d5767f9c5a82171c5df5ec2e03e6857500048a21
caionikolas <Caio.kanu@hotmail.com>
2022-10-25 23:47:36 -0300

    leetcode

--- LeetCode/In_progress/13.RomanToInteger.js
+++ LeetCode/In_progress/13.RomanToInteger.js
@@ -1,4 +1,3 @@
-/*
 var romanToInt = function(s) {
     let romano = {
         I: 1,
@@ -10,28 +9,19 @@ var romanToInt = function(s) {
         M: 1000
     }
 
-    console.log(romano.I)
-    let sum = 0
+    var sum = 0
     for (let i = 0; i < s.length; i++) {
         for (let j = 0; j < Object.keys(romano).length; j++) {
-            if (s[i] == romano[j]){
-                sum += romano.
-            }
+            if (s[i] == Object.keys(romano)[j]){
+                    sum += Object.values(romano)[j]
+                }
+            } 
         }
-    }
+    return sum
 };
 
-console.log(romanToInt(III))
+console.log(romanToInt('III'))
 
-let romano = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-}
+console.log(romanToInt('LVIII'))
 
-console.log(romano)
-*/
+console.log(romanToInt('MCMXCIV'))
---
+++ LeetCode/In_progress/234.palindromeList.js
@@ -0,0 +1,5 @@
+var isPalindrome = function(head) {
+
+};
+
+console.log(isPalindrome([1,2,2,1]))
